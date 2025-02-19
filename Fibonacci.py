21d7a71fef4cb9190e525311ed7234310be0f44a
Sandeep M K <URK24CS1059@KARUNYA.EDU>
2025-02-19 09:53:53 +0530

    There is a error

---
+++ Fibonacci.py
@@ -0,0 +1,11 @@
+x=input() #to get input
+lst=list() #creating a list
+for i in range(x): #using for loop for appending the lst(list)
+    if i == 0:
+        lst.append(0)
+    elif i == 1:
+        lst.append(1)
+    else:
+        sum=int(lst[i-2])+int(lst[i-1])
+        lst.append(sum)
+print(lst) #Printing the Fibonacci sequence
